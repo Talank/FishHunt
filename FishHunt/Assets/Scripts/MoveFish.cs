1d9aaf9d877cd65ff8b45f13ada6a44ce95249a7
Talank <talankbaral@gmail.com>
2018-07-11 14:40:46 +0545

    Move fish on mouse movement sample code added

---
+++ FishHunt/Assets/Scripts/MoveFish.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFish : MonoBehaviour
+{
+
+    public float speed;
+
+    private void Update()
+    {
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = transform.position.z;
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
