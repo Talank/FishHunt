9d6a7aee7906047f9d71ac8d158ab2aefd50c06b
Talank <talankbaral@gmail.com>
2018-07-19 14:50:59 +0545

    rotate gameobject script added

--- FishHunt/Assets/Scripts/MoveFish.cs
+++ FishHunt/Assets/Scripts/MoveFish.cs
@@ -7,12 +7,33 @@ public class MoveFish : MonoBehaviour
 
     public float speed;
 
+    //Vector3 mouse_pos;
+    //Transform target;  //Assign to the object you want to rotate
+    //Vector3 object_pos;
+    //var angle  public Transform Target;
+
+    //float;
+
     private void Update()
     {
         Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         target.z = transform.position.z;
 
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+
+
+        //rotation
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(0, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
     }
 
     public void Rotating()
