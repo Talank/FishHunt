210cbc4b3c04c24a700ed622d16101f28fd88817
Talank <talankbaral@gmail.com>
2018-07-20 16:01:14 +0545

    proper rotation of gameobject

--- FishHunt/Assets/Scripts/MoveFish.cs
+++ FishHunt/Assets/Scripts/MoveFish.cs
@@ -6,7 +6,9 @@ public class MoveFish : MonoBehaviour
 {
 
     public float speed;
-    private GameObject Fish;
+    //private GameObject Fish;
+    //float previousPositionDifferenceY;
+    float previousPositionDifferenceX;
 
     //Vector3 mouse_pos;
     //Transform target;  //Assign to the object you want to rotate
@@ -17,7 +19,9 @@ public class MoveFish : MonoBehaviour
 
     void Start()
     {
-        Fish = GameObject.Find("../Resources/Fish");
+        //Fish = GameObject.Find("../Resources/Fish");
+        //previousPositionDifferenceY = Input.mousePosition.y;
+        previousPositionDifferenceX = Input.mousePosition.x;
     }
 
     private void Update()
@@ -38,9 +42,35 @@ public class MoveFish : MonoBehaviour
         mousePos.y = mousePos.y - objectPos.y;
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        
+        if(previousPositionDifferenceX * mousePos.x < 0)
+        {
+            Vector3 theScale = transform.localScale;
+            //theScale.x *= -1;
+            theScale.y *= -1;
+            transform.localScale = theScale;
+            angle = 360 - angle;
+
+        }
+
+        /*if(previousPositionDifferenceX * mousePos.x <0)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }*/
+
+
+
+        //previousPositionDifferenceY = mousePos.y;
+        previousPositionDifferenceX = mousePos.x;
+
+        //Quaternion.rota;
+
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
-        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     public void Rotating()
