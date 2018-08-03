c16234546e16427f8854b6238eae84bb524d4b5b
Talank <talankbaral@gmail.com>
2018-08-03 23:43:30 +0545

    Fish animation, food generation, and collision added

---
+++ FishHunt/Assets/Scripts/EatFood.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatFood : MonoBehaviour {
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Food"))
+        {
+            anim.SetTrigger("open");
+            Destroy(coll.gameObject);
+        }
+    }
+}
---
+++ FishHunt/Assets/Scripts/FoodGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodGeneration : MonoBehaviour {
+    public GameObject Food;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Generate", 0, Speed);
+	}
+	
+	void Generate()
+    {
+        int x = Random.Range(0, Camera.main.pixelWidth);
+        int y = Random.Range(0, Camera.main.pixelHeight);
+
+        Vector3 Target = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
+        Target.z = 0;
+
+        Instantiate(Food, Target, Quaternion.identity);
+    }
+}
