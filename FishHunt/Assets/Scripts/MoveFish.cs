2e94ed041fc96e952b5952c935edf8b210bf095e
Talank <talankbaral@gmail.com>
2018-08-08 18:06:23 +0545

    joystick error on stopping is solved

--- FishHunt/Assets/Scripts/MoveFish.cs
+++ FishHunt/Assets/Scripts/MoveFish.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//Joystick ko sabei working xa.. except stop huda saddei -180 degree ma janxa
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -8,99 +10,75 @@ public class MoveFish : MonoBehaviour
 
     public float speed;
     Rigidbody2D rb;
-    //private GameObject Fish;
-    //float previousPositionDifferenceY;
-    float previousPositionY;
-    //bool SignChangeOnYscale;
-    string PreviouslyFacing, ToFace;
-    bool rotateFlag;
+    //float previousPositionY;
+    //string PreviouslyFacing, ToFace;
+    //bool rotateFlag;
+    float z;
+    Quaternion rot;
+
+
+    float getX, getY;
+
 
     void Start()
     {
-        rotateFlag = false;
-        PreviouslyFacing = "Right";
-        //ToFace = "Right"
+        //rotateFlag = false;
+        //PreviouslyFacing = "Right";        
         rb = gameObject.GetComponent<Rigidbody2D>();
-        previousPositionY = transform.position.y;
-        /*previousPositionDifferenceY = CrossPlatformInputManager.GetAxis("Vertical");
-        previousPositionDifferenceX = CrossPlatformInputManager.GetAxis("Horizontal");
-        SignChangeOnYscale = false;*/
+        //previousPositionY = transform.position.y;       
+        z = 0;
+        //PreviousZRotation = 0;
     }
 
     private void Update()
     {
-        //Get component of joystick current position
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y) ;
 
+        MoveMyFish();
+        RotateMyFish(currentPosition);
+        ProperScaling();
+    }
+
+    void MoveMyFish()
+    {
         Vector2 JoystickPosition = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+        rb.velocity = JoystickPosition * speed;
+        //Debug.Log("moving");
+    }
 
 
 
-        if (previousPositionY < 0)
-        {
-            ToFace = "Left";
-        }
-
-        else
-        {
-            ToFace = "Right";
-        }
-
+    void RotateMyFish(Vector2 currentPosition)
+    {
         if (rb.velocity.magnitude == 0)
-        {
-            
-            if (ToFace == "Right")
-            {
-                Vector3 theScale = transform.localScale;
-                if (theScale.y < 0.0)
-                {
-                    Debug.Log(theScale.y);
-                    theScale.y *= -1;
-                    transform.localScale = theScale;
-                }
+            return;
 
-            }
-        }
-        
-        
+        Vector2 v = rb.velocity;
+        var angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
 
-
-
-
-
-        //Get our player and transform it
-        //for moving fish
-        rb.velocity =  JoystickPosition* speed;
-
-
-
-
-
-
-
-
-
-
+    void ProperScaling()
+    {
         if (transform.rotation.eulerAngles.z > 90 && transform.rotation.eulerAngles.z < 275)
         {
-            //if (rotateFlag)
-            //{
-                Vector3 theScale = transform.localScale;
-                if(theScale.y > 0)
-                {
-                    theScale.y *= -1;
-                }
+     
+            Vector3 theScale = transform.localScale;
+            if (theScale.y > 0)
+            {
+                theScale.y *= -1;
+            }
 
-                if (theScale.x < 0)
-                {
-                    theScale.x *= -1;
-                }
+            if (theScale.x < 0)
+            {
+                theScale.x *= -1;
+            }
 
             transform.localScale = theScale;
-                //rotateFlag = false;
-            //}
+            
         }
-        // else
-        // rotateFlag = true;
+        
+
 
         else
         {
@@ -118,29 +96,6 @@ public class MoveFish : MonoBehaviour
             transform.localScale = theScale;
         }
 
-        //Debug.Log(transform.rotation.eulerAngles.z);
-
-        
-        //if horizontal is +ve, make x,y +ve
-        //if horizontal -ve, make x, +ve, y -ve;
-
-
-
-
-
-
-
-
-
-
-
-        //For rotation
-        Vector2 v = rb.velocity;
-        //Vector2 AngularVelocity;
-        
-        var angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        previousPositionY = transform.position.y;
     }
+
 }
