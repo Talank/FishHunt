3ece3bdec95ee514325b84ae2ae8df0f14553111
prajwal33 <prajwal.ady@gmail.com>
2018-08-11 21:39:17 +0545

    Pause Menu Added, Back Button Also Works Now

--- FishHunt/Assets/MainMenu.cs
+++ FishHunt/Assets/MainMenu.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+
+    public static bool quitMenuEnabled = false;
+    public GameObject mainMenuUI;
+    public GameObject quitMenuUI;
 
 	public void PlayGame()
     {
@@ -14,5 +19,31 @@ public class MainMenu : MonoBehaviour {
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           if(quitMenuEnabled)
+            {
+                disableQuitMenu();
+                mainMenuUI.SetActive(true);
+            }
+           else
+            {
+                mainMenuUI.SetActive(false);
+                enableQuitMenu();
+            }
+        }
+    }
+    void disableQuitMenu()
+    {
+        quitMenuUI.SetActive(false);
+        quitMenuEnabled = false;
+    }
+    void enableQuitMenu()
+    {
+        quitMenuUI.SetActive(true);
+        quitMenuEnabled = true;
     }
 }
